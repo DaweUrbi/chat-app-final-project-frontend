83d1615069ece8e658180d99729fbcdfd732c73c
DaweUrbi <urban.da@icloud.com>
2022-12-07 12:04:12 -0800

    Updates

---
+++ precache-manifest.4068c91a295cbeb009f23cffdb67411d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "ec2f533ff2bcf1e3e221",
+    "url": "/chat-app-final-project-frontend/static/css/main.17e561a5.chunk.css"
+  },
+  {
+    "revision": "ec2f533ff2bcf1e3e221",
+    "url": "/chat-app-final-project-frontend/static/js/main.ec2f533f.chunk.js"
+  },
+  {
+    "revision": "ff74c0fe2cef2ece3b01",
+    "url": "/chat-app-final-project-frontend/static/css/1.cbc8fc6c.chunk.css"
+  },
+  {
+    "revision": "ff74c0fe2cef2ece3b01",
+    "url": "/chat-app-final-project-frontend/static/js/1.ff74c0fe.chunk.js"
+  },
+  {
+    "revision": "4e8a14610a53d667eb7b",
+    "url": "/chat-app-final-project-frontend/static/js/runtime~main.4e8a1461.js"
+  },
+  {
+    "revision": "c92c99b65392dfa8317be3218dce4eb6",
+    "url": "/chat-app-final-project-frontend/static/media/welcome.c92c99b6.gif"
+  },
+  {
+    "revision": "466bbd979ce51ca008dec748d90a59f2",
+    "url": "/chat-app-final-project-frontend/static/media/loader.466bbd97.gif"
+  },
+  {
+    "revision": "7ccf11e3ad0a1d7088b837507cb9ee24",
+    "url": "/chat-app-final-project-frontend/index.html"
+  }
+];
