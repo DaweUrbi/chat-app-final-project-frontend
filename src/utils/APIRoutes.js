5dbd04c60d44448243453f0aecc2a59c62b04eb1
DaweUrbi <urban.da@icloud.com>
2022-12-08 10:44:06 -0800

    deploymnent commit

---
+++ src/utils/APIRoutes.js
@@ -0,0 +1,10 @@
+export const host = `http://chat-app-final-project.herokuapp.com`;
+export const registerRoute = `${host}/api/auth/register`;
+export const loginRoute = `${host}/api/auth/login`;
+export const logoutRoute = `/${host}api/auth/logout`;
+export const setAvatarRoute = `${host}/api/auth/setAvatar`;
+export const allUsersRoute = `${host}/api/auth/allUsers`;
+export const sendMessageRoute = `${host}/api/messages/addMessage`;
+export const getMessagesRoute = `${host}/api/messages/getMessages`;
+export const checkLoginRoute = `${host}/api/auth/checkLogin`;
+export const setUserRoute = `${host}/api/auth/setUser`;
